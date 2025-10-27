19b53a1c4690c93fe540cda92cec34cc78675671
jenh15 <jhuegen@siue.edu>
2025-10-26 20:21:50 -0500

    Enemies Spawn in and do Damage

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -14,7 +14,10 @@ public class Hero : MonoBehaviour
 
     [Header("Dynamic")]
     [Range(0, 4)]
-    public float shieldLevel = 1;
+    public float _shieldLevel = 1;
+    // public float shieldLevel = 1;
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
     void Awake()
     {
@@ -27,7 +30,7 @@ public class Hero : MonoBehaviour
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -43,5 +46,41 @@ public class Hero : MonoBehaviour
 
         // Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        // Debug.Log("Shield trigger hit by: " + go.gameObject.name);
+
+        // Make sure it's not the same triggering go as last time
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null)      // if the shield was triggered by an enemy decrease the level of shield by 1
+        {
+            shieldLevel--;
+            Destroy(go);
+        }
+        else
+        {
+            Debug.Log("Shield trigger hit by non-Enemy: " + go.name);
+        }
+    }
+    
+    public float shieldLevel
+    {
+        get { return (_shieldLevel); }
+        private set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            // If the shield is going to be set to less than zero
+            if (value < 0)
+            {
+                Destroy(this.gameObject);   // Destroy the Hero
+            }
+        }
     }
 }
