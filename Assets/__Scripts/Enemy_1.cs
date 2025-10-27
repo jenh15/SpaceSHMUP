5b066dee90a83ecdb0185b8decc88a40f2d41b02
jenh15 <jhuegen@siue.edu>
2025-10-26 21:05:39 -0500

    Created Enemy_1
    Enemy_1 can now appears instead of Enemy_0

--- Assets/__Scripts/Enemy_1.cs
+++ Assets/__Scripts/Enemy_1.cs
@@ -11,5 +11,34 @@ public class Enemy_1: Enemy {
     [Tooltip("Amount the ship will roll left and right with the sine wave")]
     public float waveRotY = 45;
 
-    //UNFINISHED
+    private float x0;   // The initial x value of pos
+    private float birthTime;
+
+    void Start()
+    {
+        // Set x0 to the initial x position of Enemy_1
+        x0 = pos.x;
+
+        birthTime = Time.time;
+    }
+
+    // Override the Move function on Enemy
+    public override void Move()
+    {
+        Vector3 tempPos = pos;
+        float age = Time.time - birthTime;
+        float theta = Mathf.PI * 2 * age / waveFrequency;
+        float sin = Mathf.Sin(theta);
+        tempPos.x = x0 + waveWidth * sin;
+        pos = tempPos;
+
+        // rotate a bit about y
+        Vector3 rot = new Vector3(0, sin * waveRotY, 0);
+        this.transform.rotation = Quaternion.Euler(rot);
+
+        // base.Move() still handles the movement down in y
+        base.Move();
+
+        // print(bndCheck.isOnScreen);
+    }
 }
