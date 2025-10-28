10e80647bc6797cc849dba34205fff7e99db0294
jenh15 <jhuegen@siue.edu>
2025-10-27 23:31:28 -0500

    Enemies Need to Take Multiple Shots to Destroy
    Ended right before Showing Enemy Damage on page 853 in book

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -58,7 +58,7 @@ public class Enemy : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
-        GameObject otherGO = coll.gameObject;
+        /* GameObject otherGO = coll.gameObject;
         if (otherGO.GetComponent<ProjectileHero>() != null)
         {
             Destroy(otherGO);   // Destroy the Projectile
@@ -66,6 +66,31 @@ public class Enemy : MonoBehaviour
         } else
         {
             Debug.Log("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        } */
+
+        GameObject otherGO = coll.gameObject;
+
+        // Check for collisions with ProjectileHero
+        ProjectileHero p = otherGO.GetComponent<ProjectileHero>();
+        if (p != null)
+        {
+            // Only damage this Enemy if it's on screen
+            if (bndCheck.isOnScreen)
+            {
+                // Get the damage amount from the Main WEAP_DICT
+                health -= Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
+                if (health <= 0)
+                {
+                    // Destroy this Enemy
+                    Destroy(this.gameObject);
+                }
+            }
+
+            // Destroy the ProjectileHero regardless
+            Destroy(otherGO);
+        } else
+        {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
         }
     }
 }
