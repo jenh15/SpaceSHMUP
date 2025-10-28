c5ad72dbccda8ed3c71a41e3203c67beea3ed2da
jenh15 <jhuegen@siue.edu>
2025-10-27 21:47:23 -0500

    Implemented Enemy_3

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_4 : MonoBehaviour
+public class Enemy_4 : Enemy
 {
     // Start is called before the first frame update
     void Start()
