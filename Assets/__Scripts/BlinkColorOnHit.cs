80532175f9e4e41b6ab6a04d1a90d515e140f704
JordanS490 <jordanstaggs67@gmail.com>
2025-10-29 18:19:31 -0500

    book update

--- Assets/__Scripts/BlinkColorOnHit.cs
+++ Assets/__Scripts/BlinkColorOnHit.cs
@@ -11,6 +11,7 @@ public class BlinkColorOnHit : MonoBehaviour
     [Header("Dynamic")]
     public bool showingColor = false;
     public float blinkCompleteTime;
+    public bool ignoreOnCollisionEnter = false;
 
     private Material[] materials;
     private Color[] originalColors;
@@ -37,6 +38,8 @@ public class BlinkColorOnHit : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
+        if (ignoreOnCollisionEnter) return;
+
         // Check for collisions with ProjectileHero
         ProjectileHero p = coll.gameObject.GetComponent<ProjectileHero>();
         if (p != null)
@@ -49,7 +52,7 @@ public class BlinkColorOnHit : MonoBehaviour
         }
     }
 
-    void SetColors()
+    public void SetColors()
     {
         foreach (Material m in materials)
         {
