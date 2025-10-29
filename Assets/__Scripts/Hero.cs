39e4fb4390345eec2b4dc5a03e21faa27e38b155
jenh15 <jhuegen@siue.edu>
2025-10-28 20:29:21 -0500

    Fixed Race Condition
    "Race Conditions and Script Execution Order" on page 869

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -36,6 +36,9 @@ public class Hero : MonoBehaviour
         }
 
         //fireEvent += TempFire;
+
+        ClearWeapons();
+        weapons[0].SetType(eWeaponType.blaster);
     }
 
     // Update is called once per frame
@@ -111,8 +114,27 @@ public class Hero : MonoBehaviour
         Debug.Log("Absorbed PowerUp: " + pUp.type);
         switch (pUp.type)
         {
+            case eWeaponType.shield:
+                shieldLevel++;
+                break;
 
+            default:
+                if (pUp.type == weapons[0].type)
+                {
+                    Weapon weap = GetEmptyWeaponSlot();
+                    if (weap != null)
+                    {
+                        weap.SetType(pUp.type);
+                    }
+                }
+                else
+                {
+                    ClearWeapons();
+                    weapons[0].SetType(pUp.type);
+                }
+                break;
         }
+
         pUp.AbsorbedBy(this.gameObject);
     }
     
@@ -128,6 +150,27 @@ public class Hero : MonoBehaviour
                 Destroy(this.gameObject);   // Destroy the Hero
                 Main.HERO_DIED();
             }
+        }
+    }
+
+    Weapon GetEmptyWeaponSlot()
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if (weapons[i].type == eWeaponType.none)
+            {
+                return (weapons[i]);
+            }
+        }
+
+        return (null);
+    }
+
+    void ClearWeapons()
+    {
+        foreach (Weapon w in weapons)
+        {
+            w.SetType(eWeaponType.none);
         }
     }
 }
